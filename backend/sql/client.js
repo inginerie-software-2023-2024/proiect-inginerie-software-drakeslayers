d49838c6df67808665537136373549d539f491c2
radubuzas <radu-gabriel.buzas@s.unibuc.ro>
2024-01-30 02:30:49 +0200

    20 - Changed js code

--- backend/sql/client.js
+++ backend/sql/client.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const {Client} = require('pg');
 
 const client = new Client({
@@ -8,19 +9,32 @@ const client = new Client({
     port: 5432
 });
 
+const createTableUserQuery = fs.readFileSync('users.sql', 'utf8');
+const createTableProfileQuery = fs.readFileSync('profiles.sql', 'utf8');
+const createTablePotsQuery = fs.readFileSync('posts.sql', 'utf8');
+const createTablePotLikesQuery = fs.readFileSync('postLikes.sql', 'utf8');
+const createTableCommentsQuery = fs.readFileSync('comments.sql', 'utf8');
+const createTableCommentLikesQuery = fs.readFileSync('commentLikes.sql', 'utf8');
+const createTableFollowersQuery = fs.readFileSync('followers.sql', 'utf8');
+const createTableNotificationTypeQuery = fs.readFileSync('notification-type.sql', 'utf8');
+const createTableNotifications = fs.readFileSync('notifications.sql', 'utf8');
+const createTableNotificationRecipientsQuery = fs.readFileSync('notification-recipients.sql', 'utf8');
+const createTableChatsQuery = fs.readFileSync('chats.sql', 'utf8');
+const createTableChatUsersQuery = fs.readFileSync('chatsUsers.sql', 'utf8');
+const createTableChatMessagesQuery = fs.readFileSync('chatMessages.sql', 'utf8');
+
+
 client.connect()
     .then(() => {
         console.log('Connected to the PostgreSQL database');
 
-        // const createDB = 'DROP DATABASE IF EXISTS "dbMDS"; CREATE DATABASE "dbMDS" WITH OWNER = brontosaur ENCODING = \'UTF8\' LC_COLLATE = \'en_US.utf8\' LC_CTYPE = \'en_US.utf8\' TABLESPACE = pg_default CONNECTION LIMIT = -1 IS_TEMPLATE = False;\n';
-        //
-        // client.database(createDB, (err, res) => {
-        //     if (err) {
-        //         console.error('Error creating DB', err);
-        //     } else {
-        //         console.log('DB create successfully');
-        //     }
-        // });
+        client.query(createTableUserQuery, (err, res) => {
+            if (err) {
+                console.error('Error creating DB', err);
+            } else {
+                console.log('createTableUserQuery create successfully');
+            }
+        });
 
         return client.end();
     })
